b3f9c7ec8b5a22936765a9be921d08189240fbce
ShaKQl <halimovsvhzod@gmail.com>
2023-08-02 02:32:07 +0500

    Task6 completed

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0){
+    Console.WriteLine($"{num} --> да");
+}
+else {
+    Console.WriteLine($"{num} --> нет");
+}
