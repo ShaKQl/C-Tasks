0a3f9db3fd9105ec51d7f22d9158aa3a8efd9b23
ShaKQl <halimovsvhzod@gmail.com>
2023-08-02 02:16:01 +0500

    Task4 completed

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первое число:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:  ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:  ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (max < num2) max = num2;
+if (max < num3) max = num3;
+
+Console.WriteLine($"{num1},{num2},{num3} --> {max}");
+
