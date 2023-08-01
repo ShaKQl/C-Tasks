3d2df0e2533e61cf5d711b0c0127e4e90af468c0
ShaKQl <halimovsvhzod@gmail.com>
2023-08-02 03:07:45 +0500

    Task8 completed

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Выберете число:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int start = 0;
+
+Console.Write($"{number} --> ");
+
+while (start + 1 < number){
+    start += 2;
+    Console.Write($" {start},");
+};
