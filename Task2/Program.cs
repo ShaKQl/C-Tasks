61a3938c201555f9e1fd87991a21a032058c67b2
ShaKQl <halimovsvhzod@gmail.com>
2023-08-02 01:30:30 +0500

    Task2 Completed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первое число:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:  ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2) {
+    Console.WriteLine($"Наибольшее значение: {num2} ");
+}
+else if (num1 == num2) {
+    Console.WriteLine($"Числа равны");
+}
+else {
+    Console.WriteLine($"Наибольшее значение: {num1} ");
+}
